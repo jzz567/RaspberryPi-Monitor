d0a51eb32bd39df07a5885f6541ca9b542d33f30
jzz <jzz15@vip.qq.com>
2016-04-14 10:24:50 +0800

    修复某些小问题

--- get_cpu_info.php
+++ get_cpu_info.php
@@ -3,3 +3,4 @@ echo
     "CPU:" . explode(":", exec("cat /proc/cpuinfo |grep 'model name'"))[1] .
     "<br />CPU逻辑核心数:" . exec("cat /proc/cpuinfo |grep 'processor'|wc -l") .
     "<br />CPU支持的指令集:" . explode(":", exec("cat /proc/cpuinfo |grep 'Features'"))[1];
+
